c905deb02f9e67689e72c50a2ae868a5010d0bd2
ANDRE <andredesousalucena@gmail.com>
2023-04-03 21:00:55 -0300

    Preparacao e separacao de dados

--- src/kobe_shots/pipelines/PreparacaoDados/nodes.py
+++ src/kobe_shots/pipelines/PreparacaoDados/nodes.py
@@ -4,6 +4,7 @@ generated using Kedro 0.18.7
 """
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def load_data(data):
     
@@ -13,12 +14,38 @@ def load_data(data):
 def filter_data(kobe_dataset):
 
     separador = '2PT Field Goal'
-    filter_2fg = kobe_dataset[kobe_dataset['shot_type'] == separador]
-    filter_3fg = kobe_dataset[kobe_dataset['shot_type'] != separador]
+    filter_2fg_partial = kobe_dataset[kobe_dataset['shot_type'] == separador]
+    filter_3fg_partial = kobe_dataset[kobe_dataset['shot_type'] != separador]
+
+    filter_2fg = filter_2fg_partial[['lat',
+                                     'lon',
+                                     'minutes_remaining',
+                                     'period',
+                                     'playoffs',
+                                     'shot_distance',
+                                     'shot_made_flag']]
+    filter_3fg = filter_3fg_partial[['lat',
+                                     'lon',
+                                     'minutes_remaining',
+                                     'period',
+                                     'playoffs',
+                                     'shot_distance',
+                                     'shot_made_flag']]
 
     filter_2fg = filter_2fg.dropna()
     filter_3fg = filter_3fg.dropna()
 
     return filter_2fg, filter_3fg
 
+def separe_data(data_2fg, test_size, random_state):
+
+    X = data_2fg.drop('shot_made_flag', axis=1)
+    y = data_2fg[['shot_made_flag']]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                        y, 
+                                                        test_size = test_size, 
+                                                        random_state = random_state)
+
+    return X_train, X_test, y_train, y_test
 
--- src/kobe_shots/pipelines/PreparacaoDados/pipeline.py
+++ src/kobe_shots/pipelines/PreparacaoDados/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import load_data, filter_data
+from .nodes import load_data, filter_data, separe_data
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
@@ -19,5 +19,11 @@ def create_pipeline(**kwargs) -> Pipeline:
             inputs =  'kobe_dataset',
             outputs = ['2fg_dataset','3fg_dataset'],
             name = 'filter_data'
+        ),
+        node(
+            func = separe_data,
+            inputs = ['2fg_dataset','params:test_size','params:random_state'],
+            outputs = ['X_train_data', 'X_test_data', 'y_train_data', 'y_test_data'],
+            name = 'separe_data'
         )
     ])
