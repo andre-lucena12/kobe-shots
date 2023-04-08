97b8ede1d89eff820848a000ca605e471ec49377
ANDRE <andredesousalucena@gmail.com>
2023-04-08 13:06:25 -0300

    Treinamento adicionado

--- src/kobe_shots/pipelines/Treinamento/nodes.py
+++ src/kobe_shots/pipelines/Treinamento/nodes.py
@@ -5,9 +5,28 @@ generated using Kedro 0.18.7
 
 import pandas as pd
 from pycaret.classification import *
+from sklearn.metrics import log_loss
 
-def logistic_regression_train(X_train, y_train):
+def logistic_regression_train(data_train, data_test, target, random_seed, train_size, fold_strategy, fold):
 
-    clf_model = setup(data=pd.merge(X_train, y_train))
+    stp = setup(data= data_train, 
+                target= target, 
+                session_id= random_seed, 
+                train_size= train_size,
+                fold_strategy= fold_strategy,
+                fold= fold,
+                log_experiment= True)
+    
+    add_metric('logloss', 'Log Loss', log_loss)
 
-    return
+    lr_model = create_model('lr', fold= fold)
+
+    y_true = data_test['shot_made_flag']
+    X_test = data_test.drop(columns='shot_made_flag')
+    y_pred = predict_model(lr_model, data= X_test)['Label']
+
+    score = log_loss(y_true, y_pred)
+
+    return lr_model, {
+        'Score_LogLoss_Lr_Model':{'value': score, 'step':1}
+    }
--- src/kobe_shots/pipelines/Treinamento/pipeline.py
+++ src/kobe_shots/pipelines/Treinamento/pipeline.py
@@ -4,7 +4,21 @@ generated using Kedro 0.18.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import logistic_regression_train
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func= logistic_regression_train,
+            inputs= ['data_train', 
+                     'data_test', 
+                     'param:target', 
+                     'param:random_state', 
+                     'param:train_size',
+                     'param:fold_strategy',
+                     'param:fold'],
+            outputs= ['lr_model', 'score_logloss'],
+            name= 'logistic_regression_train'
+        )
+    ])
