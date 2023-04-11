caab3d577d170b11bb759e8eeaa06ff86232c0fa
ANDRE <andredesousalucena@gmail.com>
2023-04-10 21:45:41 -0300

    Criando a função do best_model

--- src/kobe_shots/pipelines/Treinamento/nodes.py
+++ src/kobe_shots/pipelines/Treinamento/nodes.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.7
 
 import pandas as pd
 from pycaret.classification import *
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, f1_score
 
 def logistic_regression_train(data_train, data_test, target, random_seed, train_size, fold_strategy, fold):
 
@@ -31,3 +31,21 @@ def logistic_regression_train(data_train, data_test, target, random_seed, train_
     return lr_model, {
         'Score_LogLoss_Lr_Model':{'value': score, 'step':1}
     }
+
+def best_model_train(data_train, data_test, target, random_seed, train_size, fold_strategy, fold):
+
+    stp = setup(data= data_train, 
+                target= target, 
+                session_id= random_seed, 
+                train_size= train_size,
+                fold_strategy= fold_strategy,
+                fold= fold,
+                log_experiment= True,
+                experiment_name= 'best_model')
+    
+    add_metric('logloss', 'Log Loss', log_loss)
+    add_metric('f1_score', 'F1 Score', f1_score)
+
+    best_model = compare_models(fold= fold, return_train_score=True, sort= 'f1_score')
+    
+    
