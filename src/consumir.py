cfc7c3b01ee6809c981262122715c0a9dcd572cb
ANDRE <andredesousalucena@gmail.com>
2023-04-12 18:04:29 -0300

    Servindo modelo

--- src/consumir.py
+++ src/consumir.py
@@ -2,3 +2,6 @@
 Código referente a questão 8 - Onde é necessário servir o modelo e usar a biblioteca requests
 para fazer a inferência.
 '''
+import requests
+
+url = 'http://localhost:5000/invocations'
